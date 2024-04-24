a7988925c457e0d6ec73475f8642a84a39b28d6c
MAHASSADI ADOU MALICK JEAN <mahassadiadou@gmail.com>
2024-04-24 14:56:03 +0200

    couleur

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Malick',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -132,10 +132,12 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        theme: prismThemes.oceanicNext,
+        darkTheme: prismThemes.oceanicNext,
       },
     }),
 };
+
+
 
 export default config;
